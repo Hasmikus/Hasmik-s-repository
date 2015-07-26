db6a957168aba5ce1fa97da57b5d576a916e6c87
Hasmikus <hasmikus@gmail.com>
2015-07-27 00:24:59 +0400

    script.js in js folder

--- Calculator/js/script.js
+++ Calculator/js/script.js
@@ -6,14 +6,14 @@ function value(val) // Adding onclick values on current value.
 {
 document.getElementById("input1").value+=val;
 }
-function e() // Result of finished numeric functions. If there are errors type 'Error'.
+function execute() // Result of finished numeric functions. If there are errors type 'Error'.
 { 
 	try 
 	{ 
-	  c(eval(document.getElementById("input1").value)) 
+	  clear(eval(document.getElementById("input1").value));
 	} 
 	catch(e) 
 	{
-	  clear('Error') 
+	  clear('Error');
 	} 
 }

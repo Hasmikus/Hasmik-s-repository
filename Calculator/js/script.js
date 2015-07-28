0e0e423448978c818ef30cab66495e3937d37eb7
Hasmikus <hasmikus@gmail.com>
2015-07-28 22:00:02 +0400

    Not working

--- Calculator/js/script.js
+++ Calculator/js/script.js
@@ -1,16 +1,17 @@
-function clear(val) //  This function clears the display.
+ function clear (val) //  This function clears the display.
+{ var val = " ";
+document.getElementsByName('funct_space').value = val;
+}
+
+function  adding_value(val) // Adding onclick values on current value.
 {
-document.getElementById("input1").value=val;
-} 
-function value(val) // Adding onclick values on current value.
-{
-document.getElementById("input1").value+=val;
+document.getElementByName('funct_space').value+=val;
 }
 function execute() // Result of finished numeric functions. If there are errors type 'Error'.
 { 
 	try 
 	{ 
-	  clear(eval(document.getElementById("input1").value));
+	  clear(eval(document.getElementsByName('funct_space').value));
 	} 
 	catch(e) 
 	{

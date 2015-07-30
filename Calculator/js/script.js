8c6f55b2b256aeeb01d3af815a72257e8d0cf6ce
Hasmikus <hasmikus@gmail.com>
2015-07-30 13:16:54 +0400

    Working version. Some function names were wrong.

--- Calculator/js/script.js
+++ Calculator/js/script.js
@@ -1,20 +1,20 @@
- function clear (val) //  This function clears the display.
-{ var val = " ";
-document.getElementsByName('funct_space').value = val;
+function clearing (val) //  This function clears the display.
+{ var val;
+document.getElementById('input_id').value= val;
 }
 
 function  adding_value(val) // Adding onclick values on current value.
 {
-document.getElementByName('funct_space').value+=val;
+document.getElementById('input_id').value+=val;
 }
-function execute() // Result of finished numeric functions. If there are errors type 'Error'.
+function execute_result() // Result of finished numeric functions. If there are errors type 'Error'.
 { 
 	try 
 	{ 
-	  clear(eval(document.getElementsByName('funct_space').value));
+	  clearing(eval(document.getElementById('input_id').value));
 	} 
 	catch(e) 
 	{
-	  clear('Error');
+	  clearing('Error');
 	} 
 }

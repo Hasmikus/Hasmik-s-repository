9efb27e23a2019bf17bad5db1ec5a5d6c87f6db1
Hasmikus <hasmikus@gmail.com>
2015-08-05 16:42:42 +0400

    Latest version

--- Spiral/js/myscript.js
+++ Spiral/js/myscript.js
@@ -1,39 +1,37 @@
 function spiral_drawing() { 
- var x= document.getElementById("width").value;
- var y = document.getElementById("height").value;
- var m = x/10 ;
- var n = y/10;
- var d=10;
- var c = document.getElementById("myCanvas");
- var ctx = c.getContext("2d");
- var a = m*n;
+ x= document.getElementById("width").value;
+ y = document.getElementById("height").value;
+ horSteps = x/10 ;
+ vertSteps = y/10;
+ diff=10;
+ canv =document.getElementById("myCanvas");
+ ctx = canv.getContext("2d");
  x=500+x/2;
  y=250+y/2;
- q= 0;	
-   do  {
-    for (var i = 1; i <= m; i++) {	
-		
-	x-=d;
-	ctx.beginPath();
-	ctx.moveTo(x, y);
-	ctx.rect(x-5,y-5, 10, 10);
-	ctx.stroke();
-	q++;
-	alert("Next step");
+
+ while (horSteps!==0 && vertSteps!==0)   { 
+    for (var i = 1; i <= horSteps; i++) {
+		x-=diff;
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.rect(x-5,y-5, 10, 10);
+		ctx.stroke(); 
+		setTimeout(function(){var s=0; s++;}, 500);
 	}
 
-	for (var j = 1; j < n; j++) {
-	y-=d;
-	ctx.beginPath();
-	ctx.moveTo(x, y);
-	ctx.rect(x-5,y-5, 10, 10);
-	ctx.stroke();
-	q++;
-	alert("Next step");
+	for (var j = 1; j < vertSteps; j++) {
+		y-=diff;
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.rect(x-5,y-5, 10, 10);
+		ctx.stroke();
+		setTimeout(function(){var s=0; s++;}, 500);
 	}
-	d*=-1;
-	m--;
-	n--;
-
-	} while (m!=0 && n!=0);
+    diff*=-1;
+	horSteps--;
+	vertSteps--;
+  }
+alert ("Spiral is finished");
 }
+
+ 

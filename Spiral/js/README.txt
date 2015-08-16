456687183fc3f63a45ed842a046c5961a58ba14f
Hasmikus <hasmikus@gmail.com>
2015-08-16 18:24:47 +0400

    Latest

---
+++ Spiral/js/README.txt
@@ -0,0 +1,7 @@
+1. Width/Height don't divide on 10. - Example: width=125 or height=101. 
+
+
+
+
+
+1. sleep() function - http://eureka.ykyuen.info/2011/02/20/javascript-sleep-function/
--- Spiral/js/myscript.js
+++ Spiral/js/myscript.js
@@ -1,37 +1,37 @@
-function spiral_drawing() { 
- x= document.getElementById("width").value;
- y = document.getElementById("height").value;
- horSteps = x/10 ;
- vertSteps = y/10;
- diff=10;
- canv =document.getElementById("myCanvas");
- ctx = canv.getContext("2d");
- x=500+x/2;
- y=250+y/2;
-
- while (horSteps!==0 && vertSteps!==0)   { 
+function drawSpiral() { 
+ var width  = document.getElementById("width").value;
+ var height = document.getElementById("height").value;
+ var diff = 10;
+ var horSteps = width / diff ;
+ var vertSteps = height / diff;
+ var canv = document.getElementById("myCanvas");
+ var ctx = canv.getContext("2d");
+ var x = document.getElementById("myCanvas").width/"2" + width/2;   //Set x coordinate to draw first unit square.
+ var y = document.getElementById("myCanvas").height/"2" + height/2; //Set y coordinate to draw first unit square.
+ 
+ function unitSquare(x,y) {
+	ctx.beginPath();
+	ctx.moveTo(x, y);
+	ctx.rect(x-5,y-5, 10, 10);
+	ctx.stroke(); 
+ }
+while( horSteps!==0 && vertSteps!==0) {
     for (var i = 1; i <= horSteps; i++) {
 		x-=diff;
-		ctx.beginPath();
-		ctx.moveTo(x, y);
-		ctx.rect(x-5,y-5, 10, 10);
-		ctx.stroke(); 
-		setTimeout(function(){var s=0; s++;}, 500);
-	}
-
+		setTimeout(unitSquare (x,y),250);	
+	}	
 	for (var j = 1; j < vertSteps; j++) {
 		y-=diff;
-		ctx.beginPath();
-		ctx.moveTo(x, y);
-		ctx.rect(x-5,y-5, 10, 10);
-		ctx.stroke();
-		setTimeout(function(){var s=0; s++;}, 500);
-	}
-    diff*=-1;
+		setTimeout(unitSquare (x,y),250); 	
+     }
+	diff*=-1;
 	horSteps--;
 	vertSteps--;
-  }
-alert ("Spiral is finished");
+ 
 }
+alert("Spiral is finished");	
+}
+ 
+
 
  
